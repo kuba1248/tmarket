0f1574051a4b8e0452d3084d43e035be436f3c52
kuba <atalov8603@gmail.com>
2022-08-10 14:25:59 +0600

    sell/buy buttons added

--- transfer/urls.py
+++ transfer/urls.py
@@ -5,4 +5,7 @@ app_name = 'transfer'
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('myteam', views.myteam, name='myteam'),
+    path('buy/<int:id>/', views.buy, name='buy'),
+    path('sell/<int:id>/', views.sell, name='sell'),
 ]
--- transfer/views.py
+++ transfer/views.py
@@ -1,4 +1,8 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
+from rest_framework.decorators import api_view
+from django.utils import timezone
 
 from .models import Market, Myteam
 
@@ -8,3 +12,56 @@ def index(request):
     markets = Market.objects.all()
 
     return render(request, 'index.html', {'markets': markets})
+
+
+def myteam(request):
+    myteam = Myteam.objects.all()
+
+    return render(request, 'myteam.html', {'myteam': myteam})
+
+
+# @api_view(['GET'])
+def buy(request, id):
+    markets = Market.objects.all()
+    myteam = Myteam.objects.all()
+
+    marketp = Market.objects.get(id=id)
+
+    marketp.delete()
+
+    marketp.pk = None
+
+    tplayer = Myteam()
+    tplayer.name = marketp.name
+    tplayer.position = marketp.position
+    tplayer.age = marketp.age
+    tplayer.experience = marketp.experience
+    tplayer.date_buyed = timezone.now()
+
+    tplayer.save()
+
+    return render(request, 'myteam.html', {'myteam': myteam})
+
+
+# @api_view(['GET'])
+def sell(request, id):
+    markets = Market.objects.all()
+    myteam = Myteam.objects.all()
+
+    tplayer = Myteam.objects.get(id=id)
+
+    tplayer.delete()
+
+    tplayer.pk = None
+
+    marketp = Market()
+    marketp.name = tplayer.name
+    marketp.position = tplayer.position
+    marketp.age = tplayer.age
+    marketp.experience = tplayer.experience
+    marketp.date_buyed = timezone.now()
+
+    marketp.save()
+
+    return render(request, 'index.html', {'markets': markets})
+
